7260a0f405917fff38cc11f0029297ce06e69dce
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-02-24 15:01:42 +0900

    new機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,7 +15,8 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-      redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
+      # redirect_to ({ :action => 'new' }), :alert => 'New prototype was unsuccessfully created'
+      redirect_to new_prototype_path, alert: "New prototype was unsuccessfully created"
      end
   end
 
@@ -24,7 +25,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params) if proto.user_id == current_user.id
   end
 
   def show
