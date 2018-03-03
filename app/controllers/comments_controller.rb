3233cdf5b24f4473ee7ac41f3c476bc20d4e818d
土屋広大 <>
2018-03-03 16:12:34 +0900

    コメント一覧の表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,14 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @prototype.comments.new(comment_params)
+    @comment.save
     redirect_to prototype_path(@prototype)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
   end
 
   def set_prototype
