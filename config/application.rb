241a5c6118a61393837e5f93f5d03d2b2fe7507b
土屋広大 <>
2018-03-03 14:20:01 +0900

    kaminari導入
    kaminariを導入しました。

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,7 +10,8 @@ Bundler.require(*Rails.groups)
 module Miuraspace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-    
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework       false
       g.helper               false
