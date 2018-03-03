60379718da1ee64ec77b07cfdcf900b81655670b
土屋広大 <>
2018-03-03 15:25:04 +0900

    コメント送信できるようにしました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype
 
   def new
   end
 
   def create
+    @comment = @prototype.comments.new(comment_params)
+    redirect_to prototype_path(@prototype)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
 end
