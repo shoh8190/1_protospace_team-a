82011e369846de3c09788f33cdb86d943005fe21
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-03-10 12:42:23 +0900

    new画面でのJS修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,9 +15,8 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-
-      # redirect_to ({ :action => 'new' }), :alert => 'New prototype was unsuccessfully created'
       redirect_to new_prototype_path, alert: "New prototype was unsuccessfully created"
+      binding.pry
     end
   end
 
