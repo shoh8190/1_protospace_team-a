5e6529eec0ac28bda739ea758a87ed8a4b5fd26c
土屋広大 <>
2018-03-10 13:00:22 +0900

    コメントの非同期を実装

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,26 @@
+$(function(){
+  var commentList = $(".comment-list ul");
+  function appendComment(comment){
+    var html = `<li>${comment.body}</li>`
+    commentList.append(html);
+  }
+  $(".comment-submit").on("submit",function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href + '/comments';
+    $.ajax({
+      type: "POST",
+      url: href,
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      appendComment(data);
+    })
+    .fail(function(){
+      console.log("失敗しました");
+    })
+  })
+})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,17 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = @prototype.comments.new(comment_params)
-    @comment.save
-    redirect_to prototype_path(@prototype)
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to prototype_path(@prototype) }
+      format.json
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(user_id: current_user.id)
+    params.require(:comment).permit(:body).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
   end
 
   def set_prototype
