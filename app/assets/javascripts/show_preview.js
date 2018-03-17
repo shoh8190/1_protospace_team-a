ff8016266799416708cf0e0bd9a8c5c2c7c51675
shoh8190 <SHO@hoshinoshou-no-MacBook-Air.local>
2018-03-17 12:31:04 +0900

    rollback replacing ' to "

--- app/assets/javascripts/show_preview.js
+++ app/assets/javascripts/show_preview.js
@@ -1,15 +1,15 @@
 //共通機能
 function clear_preview($element){
-  if ($element.find(".preview")){
-    $element.find(".preview").remove();
+  if ($element.find('.preview')){
+    $element.find('.preview').remove();
   };
-  if ($element.find(".sub_image")){
-    $element.find(".sub_image").remove();
+  if ($element.find('.sub_image')){
+    $element.find('.sub_image').remove();
   };
 };
 
 function load_preview(ele1, ele2){
-  $(ele1).on("change","input[type="file"]", function(e) {
+  $(ele1).on('change','input[type="file"]', function(e) {
     console.log($(ele1));
     var file = e.target.files[0],
     reader = new FileReader();
@@ -20,9 +20,9 @@ function load_preview(ele1, ele2){
     reader.onload = (function(file) {
       return function(e) {
         clear_preview($preview);
-        $preview.css("background-image","none");
+        $preview.css('background-image','none');
         $preview.find(ele2).remove();
-        $preview.append($("<img>").attr({
+        $preview.append($('<img>').attr({
           src: e.target.result,
           class: "preview",
           width: "100%",
@@ -37,22 +37,22 @@ function load_preview(ele1, ele2){
 
 $(function(){
 
-  var sub_thum_size = "200px";
+  var sub_thum_size = '200px';
 
   //[new]メインサムネイル変更時
-  load_preview(".new-main-image", ".new-image");
+  load_preview('.new-main-image', '.new-image');
 
   //[new]サブサムネイル変更時
   for (var i = 0; i < 3; i++) {
-    load_preview(".new-sub-image#" + i, ".sub-image#" + i);
+    load_preview('.new-sub-image#' + i, '.sub-image#' + i);
   }
 
   //[edit]メインサムネイル変更時
-  load_preview(".edit-main-image", "#main_preview");
+  load_preview('.edit-main-image', '#main_preview');
 
   //[edit]サブサムネイル変更時
   for (var i = 1; i < 4; i++) {
-    load_preview(".edit-sub-image#" + i, ".sub-image#" + i);
+    load_preview('.edit-sub-image#' + i, '.sub-image#' + i);
   }
 
 });
