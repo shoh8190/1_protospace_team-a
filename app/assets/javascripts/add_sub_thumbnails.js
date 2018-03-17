b3b5b0dd0467cf9dbed870ac81f48610e6ba3999
shoh8190 <SHO@hoshinoshou-no-MacBook-Air.local>
2018-03-17 12:29:35 +0900

    replace ' to "

--- app/assets/javascripts/add_sub_thumbnails.js
+++ app/assets/javascripts/add_sub_thumbnails.js
@@ -1,36 +1,36 @@
-// $(function(){
+$(function(){
 
-//   list =
-//     `<li class="list-group-item col-md-4 add_sub_image_form">
-//       <div class="image-upload-plus">
-//         <span style=''>+</span>
-//       </div>
-//     </li>`;
+  list =
+    `<li class="list-group-item col-md-4 add_sub_image_form">
+      <div class="image-upload-plus">
+        <span style="">+</span>
+      </div>
+    </li>`;
 
-//   function add_new_form(addForm, hideForm){
-//     addForm.remove()
-//     hideForm.show().removeClass("hide_form").addClass("appear_form").css('display', 'block');
-//   };
+  function add_new_form(addForm, hideForm){
+    addForm.remove()
+    hideForm.show().removeClass("hide_form").addClass("appear_form").css("display", "block");
+  };
 
-//   function countList(){
-//     return $('.proto-sub-list .appear_form').length;
-//   };
+  function countList(){
+    return $(".proto-sub-list .appear_form").length;
+  };
 
-//   function appendList(){
-//     if(countList() < 3 && $('.hide_form').length){
-//      $('ul.proto-sub-list').append(list);
-//     };
-//   };
+  function appendList(){
+    if(countList() < 3 && $(".hide_form").length){
+     $("ul.proto-sub-list").append(list);
+    };
+  };
 
-//   appendList();
+  appendList();
 
-//   $(document).on('click', '.image-upload-plus', function(){
-//     addForm = $('.add_sub_image_form');
-//     hideForm = $('.hide_form:first');
-//     add_new_form(addForm, hideForm);
-//     appendList();
-//   });
+  $(document).on("click", ".image-upload-plus", function(){
+    addForm = $(".add_sub_image_form");
+    hideForm = $(".hide_form:first");
+    add_new_form(addForm, hideForm);
+    appendList();
+  });
 
-//  });
+ });
 
 
